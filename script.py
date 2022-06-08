64b9b1150c037eb399ccb1c69eb576229830a528
Gustavo Carvalho <gstcarv@gmail.com>
2022-06-08 20:30:40 -0300

    getting script data from json file

--- script.py
+++ script.py
@@ -3,24 +3,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import json
+
+with open('products.datapool.json', 'r') as f:
+    datapool = json.load(f)
 
 driver = webdriver.Chrome()
-driver.get("https://www.lojasrenner.com.br/")
+driver.get(datapool["renner_url"])
 
 search_field = driver.find_element(
-    by=By.CSS_SELECTOR, value="input[placeholder='Buscar produtos']"
+    by=By.CSS_SELECTOR, value=datapool["selectors"]["search_field"]
 )
 
-search_field.send_keys("Moletom")
+search_field.send_keys(datapool["query"]["search_query"])
 
 search_field.send_keys(Keys.RETURN)
 
 element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "[class^='ProductBox_']"))
+    EC.presence_of_element_located((By.CSS_SELECTOR, datapool["selectors"]["product_box"]))
 )
 
-results = driver.find_elements(by=By.CSS_SELECTOR, value="[class^='ProductBox_']")
+results = driver.find_elements(by=By.CSS_SELECTOR, value=datapool["selectors"]["product_box"])
 
 assert len(results) != 0
 
-driver.quit()
+driver.close()
